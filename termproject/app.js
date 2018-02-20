1a6ce50090fae990ed52161e9eaf636c65b90eac
Justin Abarquez <justinchristopher@sfs-wifi-dhcp-10-143-106-82.wifi.sfsu.edu>
2018-02-19 17:03:58 -0800

    Modify HTML/CSS

--- termproject/app.js
+++ termproject/app.js
@@ -28,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.get("/about", function(req, res) {
+  res.render("About.ejs");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
