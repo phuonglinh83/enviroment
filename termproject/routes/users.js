0053d4523e75d3df19ecc5d18a976ae6ac11c854
kgy945 <kgy945@naver.com>
2018-04-17 17:11:14 -0700

    Add user detailed page.

--- termproject/routes/users.js
+++ termproject/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    console.log("about page loaded");
+  res.render('users', { title: 'User' });
 });
-
 module.exports = router;
